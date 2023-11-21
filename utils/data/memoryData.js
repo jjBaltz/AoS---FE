1da2bd56bd0125d7a775a3be4ac2d49c65c50e2c
Jasper Baltz <nickolas.baltz@gmail.com>
2023-11-20 19:00:38 -0600

    all Memory Promises done

---
+++ utils/data/memoryData.js
@@ -0,0 +1,44 @@
+const dbUrl = 'http://localhost:7271';
+
+const getMemories = () => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/memories`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+const getSingleMemory = (id) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/memories/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const createMemory = (payload) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/memories`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
